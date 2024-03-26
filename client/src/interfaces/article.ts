5a5538ba53afb7067e14d31884196c4f1e8378cb
Heyner Cuevas <heyner.cuevas@skf.com>
2024-03-26 18:12:07 +0100

    update

--- client/src/interfaces/article.ts
+++ client/src/interfaces/article.ts
@@ -1,13 +1,6 @@
+import type { ApiArticleArticle } from "@strapi-types";
+
 export default interface Article {
-  id: number;
-  attributes: {
-    titre: string;
-    description: string;
-    image: object;
-    contenu: string;
-    slug: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
+  id: Number;
+  attributes: ApiArticleArticle["attributes"];
 }
--- client/src/lib/strapi.ts
+++ client/src/lib/strapi.ts
@@ -30,7 +30,11 @@ export default async function fetchApi<T>({
       url.searchParams.append(key, value);
     });
   }
-  const res = await fetch(url.toString());
+  const res = await fetch(url.toString(), {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.STRAPI_API_TOKEN}`,
+    },
+  });
   let data = await res.json();
 
   if (wrappedByKey) {
