5d543f101ae80481fc5c89d93c7348a387869386
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2024-04-24 21:36:32 +0200

    deploy url config cms and client

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 import { loadEnv } from "vite";
-const { PUBLIC_STRAPI_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { PUBLIC_STRAPI_URL, FULL_DEPLOY_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const deployUrl = new URL(FULL_DEPLOY_URL);
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,8 @@ export default defineConfig({
   image: {
     domains: [PUBLIC_STRAPI_URL]
   },
+  site: deployUrl.origin,
+  base: deployUrl.pathname,
   output: "hybrid",
   adapter: node({
     mode: "standalone"
