d1932506e9c1f96786ac452df549ab3d4a603127
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2024-04-25 01:10:28 +0200

    docker config

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0"
   },
-  site: deployUrl,
+  site: deployUrl.href,
   base: deployUrl.pathname,
   output: "hybrid",
   adapter: node({
