68707634fa4007feef0bc83d0108f86099785f00
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2024-05-12 18:27:07 +0200

    NestJS migration

--- utils-api/src/webhook/webhook.router.ts
+++
@@ -1,9 +0,0 @@
-import Server from '../server.js';
-import WebhookProxyController from './webhook.controller.js';
-
-function init() {
-    Server.httpServer.post('/webhook-proxy', WebhookProxyController.webhookProxy);
-    Server.logger.info('webhook router initialized.');
-}
-
-export default { init };
